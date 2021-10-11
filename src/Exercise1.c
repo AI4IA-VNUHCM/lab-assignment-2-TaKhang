7c4d666eed832d8653897dfe883a55fdb0be05e2
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-10-11 15:19:06 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -20,6 +20,12 @@ int main(int argc, char *argv[]) {
 	int result;
 	//Your codes here
 	
+	if (distance >= 0 && distance <= 2) 
+		result = 15000;
+	else if (distance >= 2 && 30 > distance) 
+		result = 15000 + (distance-2) * 8000;
+	else if (distance > 30)
+		result = 15000 + 28*8000 + (distance-30)*5000;
 	printf("%d", result);
 	return 0;
 }
