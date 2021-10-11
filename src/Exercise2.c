c4768fd9be0f674dc19dba706ca3f07dd1b8f331
ITITIU20226 <91868746+TaKhang@users.noreply.github.com>
2021-10-11 17:05:11 +0700

    Update Exercise2.c

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -15,22 +15,18 @@ ______________________________________
 #include <math.h>
 #include <stdbool.h>
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	int n = atoi(argv[1]);
-	int result=1;
+
 	//Your codes here
 	
-	if (n > 0)
-	{
-        int i = n % 2 == 0 ? 2 : 1;
-        while(i <= n)
-		{
-            result *= i;
-            i += 2;
-        }
-    }
-	
-	printf("%d", result);
-	return 0;
-}
+	unsigned int doublefactorial(unsigned int n) 
+{ 
+    if (n == 0 || n==1) 
+      return 1; 
+    return n*doublefactorial(n-2); 
+} 
+   
+int main() 
+{ 
+    printf("Double factorial is %d", doublefactorial(11)); 
+    return 0; 
+} 
